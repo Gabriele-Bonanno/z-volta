97cf41f3608f8166e767a5d868597bd71586f545
tommaso1411 <tommy.scotti2005@gmail.com>
2025-02-21 12:53:50 +0100

    gghj

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         die("Errore di connessione: " . $conn->connect_error);
     }
     
-    // Recupera username e password inviati dal form
+    // Recupera username e password inviati dal formm
     $username = $_POST['username'];
     $password = $_POST['password'];
     
